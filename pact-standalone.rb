649f053a0c9afd575fc3aa9a057721101c8180bf
pact-dev <pact-dev@users.noreply.github.com>
2025-07-25 15:41:10 +0000

    chore(release): Update version to v2.5.2

--- pact-standalone.rb
+++ pact-standalone.rb
@@ -1,25 +1,25 @@
 class PactStandalone < Formula
   desc "Standalone pact CLI executable using the Ruby Pact impl and Traveling Ruby"
   homepage "https://github.com/pact-foundation/pact-standalone"
-  version "2.5.1"
+  version "2.5.2"
   on_macos do
     on_intel do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.1/pact-2.5.1-osx-x86_64.tar.gz"
-      sha256 "77730ff5093d913e6125de3e4ae1768caf871a54b8cb2d8fd2fe3bb49de14c85"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.2/pact-2.5.2-osx-x86_64.tar.gz"
+      sha256 "9886297b15b261828588e4820963b633f477d5c3598ce1ca068ac315b7c328b4"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.1/pact-2.5.1-osx-arm64.tar.gz"
-      sha256 "875aa190e363b45f481b9b975bb7e80ba8bd974a07e13c08537e672ff04cad3a"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.2/pact-2.5.2-osx-arm64.tar.gz"
+      sha256 "c7f08ac263e3676ec3293d5b707b4e87c1ba1cb00289c651583a77dd37d9b4a7"
     end
   end
   on_linux do
     on_intel do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.1/pact-2.5.1-linux-x86_64.tar.gz"
-      sha256 "852efd180b5e8cff7fe97952edc3626f62b992b9ed83f332f1d8c6479a9db689"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.2/pact-2.5.2-linux-x86_64.tar.gz"
+      sha256 "3cdad4af4f65a146f27944440e488228124188e58801008ee2053129e0eaa1c7"
     end
     on_arm do
-      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.1/pact-2.5.1-linux-arm64.tar.gz"
-      sha256 "267017430094430eac78dc20212c9d43c6bbd17771057f76cb0dbe7649e765fc"
+      url "https://github.com/pact-foundation/pact-standalone/releases/download/v2.5.2/pact-2.5.2-linux-arm64.tar.gz"
+      sha256 "fe932e4a9c195624200cde6915a75c02659a465c38763a2535ce4a6e665c9dd9"
     end
   end
 
